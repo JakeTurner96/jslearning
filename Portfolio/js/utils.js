da58103508d54d59288297a200e2533ab80599ae
Jake <jaketurner810@gmail.com>
2020-02-21 18:50:11 +0000

    Change to portfolio commit

---
+++ Portfolio/js/utils.js
@@ -0,0 +1,3 @@
+function loadNav(){
+    document.write("<nav><ul><li><a href='index.html'>Home</a></li></li><li><a href='aboutMe.html'>About me</a><li><a href='projects.html'>Projects</a></div></li></ul></nav>"); 
+}
